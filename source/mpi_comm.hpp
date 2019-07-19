e3d71b2e1723804f8916d1231e3876e2556bde32
Elad Steinberg <eladtan1@Gmail.com>
2019-07-18 20:16:50 -0400

    changed number of levels

--- source/mpi_comm.hpp
+++ source/mpi_comm.hpp
@@ -8,7 +8,7 @@
 #include <array>
 #include "ExactRS.hpp"
 
-#define NGHOSTCELLS 6
+#define NGHOSTCELLS 12
 
 std::array<Primitive, NGHOSTCELLS*2> SendRecvPrimitive(std::vector<Primitive> const& cells);
 
